cef266ee8428f4fcc23cd7987af3e48670b63d26
Nikita <kkuygrttyyy@gmail.com>
2025-11-29 18:30:28 +0200

    Add files via upload

---
+++ AdapterLab/LibraryConnection.cs
@@ -0,0 +1,36 @@
+﻿using AdapterLibrary;
+
+namespace AdapterLab
+{
+   
+    public class LibraryConnection
+    {
+        private static LibraryConnection? _instance;
+
+        
+        private LibraryConnection()
+        {
+            
+        }
+
+        
+        public static LibraryConnection Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new LibraryConnection();
+                }
+                return _instance;
+            }
+        }
+
+       
+        public IDigitalTime CreateAdapter(double hourAngle, double minuteAngle)
+        {
+            var watch = new AnalogWatch(hourAngle, minuteAngle);
+            return new WatchAdapter(watch);
+        }
+    }
+}
---
+++ AdapterLab/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using AdapterLibrary;
+
+namespace AdapterLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Lab 3: Adapter + Singleton + DLL ===\n");
+
+            Console.WriteLine("Connecting to library via Singleton...");
+            var connection = LibraryConnection.Instance;
+
+            IDigitalTime digitalClock = connection.CreateAdapter(90, 180);
+
+            Console.WriteLine($"Digital Output: {digitalClock.GetTime()}");
+
+            Console.ReadKey();
+        }
+    }
+}
